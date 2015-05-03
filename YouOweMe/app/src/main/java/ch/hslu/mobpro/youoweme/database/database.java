fe70ac6059581d02a64d8ea92470bc263bb7ab7f
LivioKunz <livio_kunz@hotmail.com>
2015-05-03 15:35:39 +0200

    Added Database Package and dummy class

---
+++ YouOweMe/app/src/main/java/ch/hslu/mobpro/youoweme/database/database.java
@@ -0,0 +1,11 @@
+package ch.hslu.mobpro.youoweme.database;
+
+/**
+ * Created by livio on 03.05.2015.
+ */
+public class database {
+
+    public void dummytest(){
+        System.out.println("Test");
+    }
+}
