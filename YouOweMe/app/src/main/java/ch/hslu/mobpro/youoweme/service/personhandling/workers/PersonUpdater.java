2f680aa374b699aa79c2be07ec8e13d81a559cb2
neidi <simon.neidhart@hotmail.com>
2015-05-04 19:24:32 +0200

    PersonUpdater aktualisiert

---
+++ YouOweMe/app/src/main/java/ch/hslu/mobpro/youoweme/service/personhandling/workers/PersonUpdater.java
@@ -0,0 +1,37 @@
+package ch.hslu.mobpro.youoweme.service.personhandling.workers;
+
+import ch.hslu.mobpro.youoweme.database.EntityPersistor;
+import ch.hslu.mobpro.youoweme.database.Person;
+
+/**
+ * Created by simonneidhart on 04.05.15.
+ * Klasse, die Personen aktualisiert.
+ */
+public class PersonUpdater {
+
+    private static PersonUpdater instance;
+    private final EntityPersistor entityPersistor;
+
+    PersonUpdater(EntityPersistor entityPersistor){
+        this.entityPersistor = entityPersistor;
+    }
+
+    /**
+     * Singletonmethode
+     * @return die Instanz
+     */
+    public static PersonUpdater getInstance() {
+        if(instance==null){
+            //instance = new PersonUpdater(new EntityPersistorImpl());
+        }
+        return instance;
+    }
+
+    /**
+     * Aktualisiert eine Person anhand der im Objekt gespeicherten Daten
+     * @param person das Personenobjekt mit den zu aktualisierenden Daten.
+     */
+    public void updatePerson(Person person) {
+
+    }
+}
